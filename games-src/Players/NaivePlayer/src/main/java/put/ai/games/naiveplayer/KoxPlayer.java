4f1dd3cfdbb5d48390840514bd3f5808f41343d6
Luber90 <luberon90@gmail.com>
2021-01-29 13:12:58 +0100

    kox

--- games-src/Players/NaivePlayer/src/main/java/put/ai/games/naiveplayer/KoxPlayer.java
+++ games-src/Players/NaivePlayer/src/main/java/put/ai/games/naiveplayer/KoxPlayer.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 import put.ai.games.game.Board;
 import put.ai.games.game.Move;
 import put.ai.games.game.Player;
@@ -16,22 +18,45 @@ import static java.lang.StrictMath.*;
 
 public class KoxPlayer extends Player {
 
-    private Random random = new Random(0xdeadbeef);
+    private class dwaInt{
+        public int a, b;
+        public dwaInt(int x, int y){
+            a = x;
+            b = y;
+        }
+    }
 
     private int distToCentr(int x1, int y1){
-        return (int)sqrt(pow(3.5-x1,2)+pow(3.5-y1,2));
+        return 5-(int)sqrt(pow(3.5-x1,2)+pow(3.5-y1,2));
     }
 
     private int eval(Board b, Move move, Color color) {
         int eval = 0;
         int size = b.getSize();
+        int ile = 0;
+        List<dwaInt> values = new ArrayList<dwaInt>();
         for(int i = 0; i < size; i++){
             for(int j = 0; j < size; j++){
-                if(b.getState(i,j) == getColor()) eval += distToCentr(i, j);
-                else if(b.getState(i,j) == getOpponent(getColor())) eval -= distToCentr(i, j);
+                if(b.getState(i,j) == getColor()){
+                    ile = 0;
+                    eval += distToCentr(i, j);
+                    for (int p = -1; p < 2; p++) {
+                        for (int o = -1; o < 2; o++) {
+                            if (p != 0 && o != 0 && i + p > 0 && j + o > 0) {
+                                if (b.getState(i+p, j + o)==getColor()) {
+                                    ile++;
+                                }
+                            }
+                        }
+                    }
+                    eval += ile;
+                }
+                else if(b.getState(i,j) == getOpponent(getColor())){
+                    eval -= distToCentr(i, j);
+                }
             }
         }
-        return -eval;
+        return eval;
     }
 
     @Override
@@ -50,6 +75,14 @@ public class KoxPlayer extends Player {
             values.add(d);
             System.out.println("Move: " + m + " = " + d);
             b.undoMove(m);
-        }return moves.get(values.indexOf(Collections.max(values)));
+        }
+        /*
+        try{
+            TimeUnit.SECONDS.sleep(1);
+        }
+        catch (java.lang.InterruptedException e){
+
+        }*/
+        return moves.get(values.indexOf(Collections.max(values)));
     }
 }
